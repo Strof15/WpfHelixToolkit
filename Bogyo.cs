1a7d9ede86653946949d86143e714ced5c807897
Strof <robertnagykristof@gmail.com>
2024-03-27 12:01:50 +0100

    Add project files.

---
+++ Bogyo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHelixToolkit
+{
+    public class Bogyo
+    {
+        int x;
+        int y;
+        int z;
+        int e;
+
+        public Bogyo(String sor)
+        {
+            string [] adatok = sor.Split(";");
+            this.X = int.Parse(adatok[0]);
+            this.Y = int.Parse(adatok[1]);
+            this.Z = int.Parse(adatok[2]);
+            this.E = int.Parse(adatok[3]);
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Z { get => z; set => z = value; }
+        public int E { get => e; set => e = value; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using HelixToolkit.Wpf;
+
+namespace WpfHelixToolkit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> sorok = File.ReadAllLines("gyongyok.txt").Skip(1).ToList();
+            List<Bogyo> bogyoklistaja = new();
+
+            foreach (string sor in sorok)
+            {
+                bogyoklistaja.Add(new Bogyo(sor));
+            }
+            foreach (Bogyo bogyo in bogyoklistaja)
+            {
+                EllipsoidVisual3D peldabogyo = new();
+                peldabogyo.Center = new Point3D(bogyo.X, bogyo.Y, bogyo.Z);
+                peldabogyo.Fill = new SolidColorBrush(Colors.Beige);
+                ter.Children.Add(peldabogyo);
+            }
+
+        }
+    }
+}
